5433aad3ea4ebb737a9d5e0900da3f19515c853a
Pedro <up202008467@up.pt>
2025-04-10 15:29:03 +0100

    Test

--- pages/profile.php
+++ pages/profile.php
@@ -1,1 +1,20 @@
-idk
+<?php function draw_header($title) { ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><?=$title?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/style.css">
+  </head>
+  <body>
+    <h1><?=$title?></h1>
+    <main>
+<?php } ?>
+
+<?php function draw_footer($title) { ?>
+    </main>
+  </body>
+  <h1><?=$title?></h1>
+</html>
+<?php } ?>
