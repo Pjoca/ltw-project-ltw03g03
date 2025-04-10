395c9d1a6bb94066b4d66f04b25ada6016ab8f85
Pedro <up202008467@up.pt>
2025-04-10 15:34:10 +0100

    Test

--- pages/profile.php
+++ pages/profile.php
@@ -1,20 +1,3 @@
-<?php function draw_header($title) { ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <title><?=$title?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="../css/style.css">
-  </head>
-  <body>
-    <h1><?=$title?></h1>
-    <main>
-<?php } ?>
+<?php 
 
-<?php function draw_footer($title) { ?>
-    </main>
-  </body>
-  <h1><?=$title?></h1>
-</html>
-<?php } ?>
+?>
