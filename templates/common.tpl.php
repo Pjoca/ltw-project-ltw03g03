033f107a0b9137661245da90dd6ba30e213ac952
Pedro <up202008467@up.pt>
2025-05-22 13:45:54 +0100

    Home Banner ajustment

--- templates/common.tpl.php
+++ templates/common.tpl.php
@@ -59,6 +59,7 @@
   <div class="back-nav">
     <a href="/../pages/profile.php" class="nav-button"> Profile </a>
     <a href="/../pages/my.services.php" class="nav-button"> My Services </a>
+    <a href="/../pages/search.php" class="nav-button"> Search </a>
     <a href="logout.php"class="nav-button"> Logout</a>
   </div>
 
